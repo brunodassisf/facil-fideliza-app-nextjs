d5bb26607f33ba33fdc64a1cb57154f9330deb52
Bruno de Assis Ferreira <33126607+brunodassisf@users.noreply.github.com>
2025-01-17 01:54:53 -0300

    refactor(app): general settings

--- src/app/(client)/[tag]/layout.tsx
+++ src/app/(client)/[tag]/layout.tsx
@@ -1,10 +1,29 @@
-import { checkStoreTag } from "@/core/actions/store";
+import { checkStoreTag, getStore } from "@/core/actions/store";
 import { NotFoundStore, WrapperTag } from "@/presentation/components";
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 
-export const metadata: Metadata = {
-  title: "Fácil Fidelizar - Login",
-};
+export async function generateMetadata(): Promise<Metadata> {
+  const store = await getStore();
+  const img = store.img
+    ? process.env.NEXT_PUBLIC_URL_BASE_IMAGE! + store.img
+    : "";
+
+  return {
+    title: `${store.name}, login`,
+
+    openGraph: {
+      url: `${process.env.NEXT_PUBLIC_URL_APP}/loja`,
+      images: [img],
+    },
+  };
+}
+
+export async function generateViewport(): Promise<Viewport> {
+  const store = await getStore();
+  return {
+    themeColor: store.bgColor || "#83cbff",
+  };
+}
 
 export default async function RootLayout({
   children,
---
+++ src/app/(client)/[tag]/loading.tsx
@@ -0,0 +1,5 @@
+import { Spinner } from "@/presentation/components";
+
+export default function Loading() {
+  return <Spinner />;
+}
